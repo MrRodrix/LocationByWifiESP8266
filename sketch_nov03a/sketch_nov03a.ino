856303aea75105ba1964f490262406f8a4d8250a
Rodrigues <51835611+MrRodrix@users.noreply.github.com>
2021-11-24 23:07:46 -0300

    send info to server

--- sketch_nov03a/sketch_nov03a.ino
+++ sketch_nov03a/sketch_nov03a.ino
@@ -1,46 +1,55 @@
 #include <ESP8266WiFi.h>
-//#include <ESP8266HTTPClient.h>
+#include <ESP8266HTTPClient.h>
 #include <WifiLocation.h>
 
 
 const String key = "AIzaSyANCTMJZQJl1K3Qi_l0jkijdw4xMN-vQNI";
 const char* ssid = "DTEL_SOARES";
 const char* passwd = "lidiane20";
-const String servername = "www.googleapis.com/geolocation/v1";
+const String serverPath = "http://onde-circular.herokuapp.com/wifi";
 WiFiClient cliente;
 WifiLocation location(key);
+HTTPClient http;
 
-void setup() {
-  Serial.begin(9600);
-  Serial.println("Location request data");
-  // Connect to WPA/WPA2 network
+void connect_wifi(){
+  // Connect on wifi
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, passwd);
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.println("Attempting to connect to WPA SSID: ");
+    Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
     // wait 5 seconds for connection:
-    Serial.println("Status = ");
+    Serial.print("Status = ");
     Serial.println(WiFi.status());
     delay(500);
   }
-  Serial.println("Status = ");
+  Serial.print("End\n Status = ");
   Serial.println(WiFi.status());
-  Serial.println("Setup finalizado");
 }
-//HTTPClient http;
+
+void setup() {
+  Serial.begin(9600);
+  connect_wifi();
+}
 
 void loop() {
+  if (WiFi.status() != WL_CONNECTED){
+    connect_wifi();
+  }
   Serial.println("STATUS WIFI: "+ String(WiFi.status()));
   Serial.println("Location request data");
   Serial.println(location.getSurroundingWiFiJson());
-
-//  String serverPath = "google.com";
-//  if (cliente.connect(serverPath, 443)){
-//    Serial.println("connected");
-//    cliente.println("GET /search?q=arduino HTTP/1.0");
-//    cliente.println();
-//  }
-//  
+  String data = "{\"wifiAccessPoints\": "+String(location.getSurroundingWiFiJson())+"}";
+  http.begin(cliente, serverPath);
+  // If you need an HTTP request with a content type: application/json, use the following:
+  http.addHeader("Content-Type", "application/json");
+  // JSON data to send with HTTP POST            
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(data);
+  Serial.print("HTTP Response code: ");
+  Serial.println(httpResponseCode);
+    
+  // Free resources
+  http.end();
   delay(10000);
 }
